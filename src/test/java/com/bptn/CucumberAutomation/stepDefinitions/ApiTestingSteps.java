8bada7f741aacac0f35ad911eaf39eaa50cfb7b4
Sweytha <73808022+Sweytha@users.noreply.github.com>
2023-07-21 13:34:34 -0400

    Update ApiTestingSteps.java

--- src/test/java/com/bptn/CucumberAutomation/stepDefinitions/ApiTestingSteps.java
+++ src/test/java/com/bptn/CucumberAutomation/stepDefinitions/ApiTestingSteps.java
@@ -1,7 +1,14 @@
 package com.bptn.CucumberAutomation.stepDefinitions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,9 +16,15 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+
 public class ApiTestingSteps {
 
+	
+	 
 	Response response;
+	
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Given("the base URL is {string}")
 	public void the_base_url_is(String baseURL) {
@@ -30,7 +43,6 @@ public class ApiTestingSteps {
 				.then()
 				.extract()
 				.response();
-
 	}
 
 	@Then("the response status code should be {int}")
@@ -38,12 +50,12 @@ public class ApiTestingSteps {
 		assertEquals(statusCode, this.response.getStatusCode());
 	}
 
-	@Then("the title of the second post should be {string}")
+	@And("the title of the second post should be {string}")
 	public void the_title_of_the_second_post_should_be(String expectedTitle) {
 
-		assertEquals(expectedTitle, response.jsonPath().getString("title[1]"));
+		assertEquals(expectedTitle, this.response.jsonPath().getString("title[1]"));
 	}
-	
+
 	@When("I perform a GET request to {string} with the query parameter {string}")
 	public void i_perform_a_get_request_to_with_the_query_parameter(String endpoint, String queryParam) {
 		this.response = RestAssured.given()
@@ -56,10 +68,43 @@ public class ApiTestingSteps {
                 .response();
 	}
 
-	@Then("the email at index {int} should be {string}")
+	@And("the email at index {int} should be {string}")
 	public void the_email_at_index_should_be(int index, String expectedEmail) {
-		assertEquals(expectedEmail, response.jsonPath().getString("email[" + index + "]"));
+		assertEquals(expectedEmail, this.response.jsonPath().getString("email[" + index + "]"));
 	}
 
+	@When("I perform a POST request to {string} with the following data")
+	public void i_perform_a_post_request_to_with_the_following_data(String endpoint, DataTable data) throws JsonProcessingException{
+		   this.response = RestAssured.given()
+	                .contentType(ContentType.JSON)
+	                .body(this.objectMapper.writeValueAsString(data.asMap()))
+	                .when()
+	                .post(endpoint)
+	                .then()
+	                .extract()
+	                .response();
+		
+	}
 
+	@And("the title should be {string}")
+	public void the_post_title_should_be(String expectedTitle) {
+		assertEquals(expectedTitle, this.response.jsonPath().getString("title"));
+	}
+
+	@And("the body should be {string}")
+	public void the_post_body_should_be(String expectedBody) {
+		assertEquals(expectedBody, this.response.jsonPath().getString("body"));
+	}
+
+	@And("the userId should be {string}")
+	public void the_post_userId_should_be(String expectedUserId) {
+		assertEquals(expectedUserId, this.response.jsonPath().getString("userId"));
+	}
+
+	@And("the id should be a positive integer")
+	public void the_post_id_should_be_positive_integer() {
+		int id = this.response.jsonPath().getInt("id");
+		assertTrue(id > 0, "Id should be a positive integer.");
+	}
+	
 }
